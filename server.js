27e5f0c3813839e7bc731034c5c015ce36a181ca
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-26 05:46:59 -0700

    Fixed again?

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const PORT = proces.env.PORT || 8000;
+
 const express = require('express');
+const PORT = proces.env.PORT || 5000;
 const app = express();
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 app.use(express.static('public'));
