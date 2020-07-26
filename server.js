23543da289d4013b1766cd48b7eb60a9954ce339
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-26 13:18:27 -0700

    Modularized REST API

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const app = express();
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
-app.post('/query', (req, res) => {
-  console.log(req.body);
-  res.end(JSON.stringify(req.body));
+app.post('/query_post', (req, res) => {
+  res.send(req.body);
+  res.end();
 });  
+app.get('/query_get', (req, res) => {
+  res.send("Gotten");
+  res.end();
+});
+
+
+// app.get(url, onQuery(input, output));
